44404d1f0f19bc7dc6f3eb37acef2e7b04215490
Aeri <rjungdesign@gmail.com>
2020-09-30 23:50:32 -0400

    add message list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
-// import useMessage from "./hook/useMessage";
 import { inputType, socketIo } from "./constants";
 
 const socket = io("http://localhost:4000/");
 const currentUser = `User${Math.floor(Math.random() * 1000000)}`;
 
 function App() {
-  const [message, setMessage] = useState();
+  const [message, setMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
 
+  // todo create custom hook
   useEffect(() => {
     socket.emit(socketIo.newUser, currentUser);
 
@@ -34,15 +34,22 @@ function App() {
   const onSubmit = event => {
     event.preventDefault();
     socket.emit(socketIo.chatMessage, message);
-    socket.on(socketIo.chatMessage, msg => {
-      console.log("frontend: ", msg);
-    });
+    setMessage("");
+  };
+
+  const renderMessage = () => {
+    return messageList.map((msg, index) => (
+      <li key={index}>
+        user: {msg.user} / message: {msg.message}
+      </li>
+    ));
   };
 
   return (
     <div>
+      <ul>{messageList.length > 0 && renderMessage()}</ul>
       <form onSubmit={onSubmit}>
-        <input type={inputType.text} onChange={event => setMessage(event.target.value)} />
+        <input type={inputType.text} onChange={event => setMessage(event.target.value)} value={message} />
         <button type={inputType.submit}>Submit</button>
       </form>
     </div>
